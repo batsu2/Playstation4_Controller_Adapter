9789db36ad974fdf69461d4d40af04b097dc3129
Bryan <44759525+batsu2@users.noreply.github.com>
2022-11-29 03:36:37 -0600

    Update PS4Controller.java

--- PS4Controller.java
+++ PS4Controller.java
@@ -12,17 +12,18 @@ public class PS4Controller
     public static final int BUTTON_CIRCLE = 1;
     public static final int BUTTON_SQUARE = 2;
     public static final int BUTTON_TRIANGLE = 3;
-    public static final int BUTTON_L1 = 4;
-    public static final int BUTTON_R1 = 5;
-    public static final int BUTTON_BACK = 7;
+    public static final int SHARE_BUTTON = 4;
+    public static final int PS_BUTTON = 5;
     public static final int BUTTON_START = 6;
-    public static final int BUTTON_LS = 8; //Left Stick pressed down
-    public static final int BUTTON_RS = 9; //Right Stick pressed down
+    public static final int BUTTON_LS = 7; //Left Stick pressed down
+    public static final int BUTTON_RS = 8; //Right Stick pressed down
+    public static final int BUTTON_L1 = 9;
+    public static final int BUTTON_R1 = 10;
 
-    public static final int D_PAD_LEFT = 13;
-    public static final int D_PAD_RIGHT = 14;
     public static final int D_PAD_UP = 11;
     public static final int D_PAD_DOWN = 12;
+    public static final int D_PAD_LEFT = 13;
+    public static final int D_PAD_RIGHT = 14;
 
     public static final int POV = 0;
 
